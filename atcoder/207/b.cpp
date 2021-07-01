19eb397f1b0c77f20e86b42a4a83d83a6a95e0db
zhupeijun <zhupeijun2006@163.com>
2021-07-01 22:45:47 +0900

    [AC] ABC207 abc

---
+++ atcoder/207/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+
+#define rep(i, n) for (int i=0; i<(n); ++i)
+#define each(v, i) for (auto i : v) 
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define maxs(x, y) x = max(x, y)
+#define mins(x, y) x = min(x, y)
+#define pcnt __builtin_popcount
+#define rng(a) a.begin(), a.end()
+
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vector<vector<T>>;
+using vi = vc<int>;
+
+int main() {
+    ll A, B, C, D; cin>>A>>B>>C>>D;
+    ll X = C*D-B;
+    if (X <= 0) cout << -1 << endl;
+    else cout << (A + (X-1)) / X << endl; 
+    return 0;
+}
