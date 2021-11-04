79aaf2e24a6d00136326d03670ff8b0c0d77bf12
Peijun Zhu <peijun.zhu@bytedance.com>
2021-11-04 14:29:55 +0900

    [AC] 225 A

---
+++ atcoder/225/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+
+#define rep(i, n) for (int i=0; i<(n); ++i)
+#define each(v, i) for (auto i : v) 
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define maxs(x, y) x = max(x, y)
+#define mins(x, y) x = min(x, y)
+#define pcnt __builtin_popcount
+#define rng(a) a.begin(), a.end()
+
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vector<vector<T>>;
+using vi = vc<int>;
+
+int main() {
+    string s; cin >> s;
+    set<char> d(s.begin(), s.end());
+    vector<int> M = {0, 1,3,6};
+    cout << M[sz(d)] << endl;
+    return 0;      
+}
